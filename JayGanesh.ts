f46f4b24d5aa1605f1adee42b55006eec5474fab
Shubham Lodha <83054906+shubhamlodha21@users.noreply.github.com>
2021-07-31 00:05:29 +0530

    Program to print Jay Ganesh..!

---
+++ JayGanesh.ts
@@ -0,0 +1,8 @@
+//Function Name : JayGanesh
+//Input:--
+//Output: Jay Ganesh..!
+//Description: Program to print Jay Ganesh..!
+//Author: Shubham Lodha
+//Date: 31/07/2021
+
+console.log("Jay Ganesh..!");
