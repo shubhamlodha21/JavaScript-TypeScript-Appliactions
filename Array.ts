4b6f8a56b1c9961cb30eb153bf7b887e955759ec
Shubham Lodha <83054906+shubhamlodha21@users.noreply.github.com>
2021-08-01 08:37:43 +0530

    Implementations of Array

---
+++ Array.ts
@@ -0,0 +1,18 @@
+//Function Name :--
+//Input: --
+//Output:
+/*object
+[ 1, 2, 3, 4, 5 ]
+1*/
+
+//Description: Implemetations of Arrays
+
+//Author: Shubham Lodha
+//Date: 01/08/2021
+
+
+var arr1:number[];
+arr1=[1,2,3,4,5];
+console.log(typeof(arr1));//Object
+console.log(arr1);
+console.log(arr1[0]);//0th 
