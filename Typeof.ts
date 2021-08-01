c30c9a8a83bc935c91741377b76171f4763ca3da
Shubham Lodha <83054906+shubhamlodha21@users.noreply.github.com>
2021-08-01 08:42:15 +0530

    Program to Display Datatypes of variables

---
+++ Typeof.ts
@@ -0,0 +1,17 @@
+//Function Name :--
+//Input: --
+//Output:Number,string,undefined
+
+//Description: Program to Display Datatypes of variables
+
+//Author: Shubham Lodha
+//Date: 01/08/2021
+
+var no = 101
+console.log(typeof no);
+
+var str = "Marvellous Infosystems"
+console.log(typeof str);
+
+var value;
+console.log(typeof value);
