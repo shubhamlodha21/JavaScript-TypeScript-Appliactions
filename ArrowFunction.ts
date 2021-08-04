90af352f6b4f984a99716db9642a22289c0d0661
Shubham Lodha <83054906+shubhamlodha21@users.noreply.github.com>
2021-08-04 14:02:35 +0530

    Used of Arrow Function

---
+++ ArrowFunction.ts
@@ -0,0 +1,22 @@
+//Function Name :ArrowFunctions
+//Input: --
+/*Output: 
+111
+Inside Infosystems
+100*/
+
+//Description: Used of Arrow Function
+//Author: Shubham Lodha
+//Date: 04/08/2021
+
+
+var Marvellous = (x:number)=> 10 + x 
+console.log(Marvellous(101));
+
+var Infosystems = (x:number)=> {    
+    console.log("Inside Infosystems");
+    console.log(x)  
+ } 
+ Infosystems(100);
+
+
