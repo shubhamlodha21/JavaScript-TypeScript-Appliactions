b29906687e28db580a61b4cf65f5d84fe60aed0c
Shubham Lodha <83054906+shubhamlodha21@users.noreply.github.com>
2021-08-04 14:02:01 +0530

    Used of AnonymousFunction

---
+++ AnonymousFunction.ts
@@ -0,0 +1,14 @@
+//Function Name :AnonymousFunction
+//Input: --
+//Output:11
+//Description: Used of AnonymousFunction
+//Author: Shubham Lodha
+//Date: 04/08/2021
+
+
+var iret = function(no1:number,no2:number) 
+{ 
+    return no1 + no2;  
+}; 
+
+console.log(iret(8,3));
