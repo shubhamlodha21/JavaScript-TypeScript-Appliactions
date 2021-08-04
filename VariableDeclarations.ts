d4f8fb00f9d56e136ab3ebe8cd9ead9598e60116
Shubham Lodha <83054906+shubhamlodha21@users.noreply.github.com>
2021-08-04 14:05:11 +0530

    Variable Declarations in typescript

---
+++ VariableDeclarations.ts
@@ -0,0 +1,12 @@
+//Function Name :--
+//Description: Variable Declarations in typescript
+//Author: Shubham Lodha
+//Date: 04/08/2021
+
+var name1:string = "Marvellous Infosystems";
+
+var name2:string;
+
+var name3 = "Marvellous Infosystems";
+
+var name4;
