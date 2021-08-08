2f45ff64cb187b0560de13abd2094209799ad1fa
Shubham Lodha <83054906+shubhamlodha21@users.noreply.github.com>
2021-08-08 21:38:43 +0530

    Program to Perform Addition and Subtraction of Number Logic to export for Client

---
+++ Server.ts
@@ -0,0 +1,18 @@
+//Function Name :Addition,Substraction
+//Description: Program to Perform Addition and Substraction of Number Logic to export for Client 
+//Author: Shubham Lodha
+//Date: 08/08/2021
+
+export function Addition(no1:number,no2:number):number
+{
+    var ans=0
+    ans=no1+no2
+    return ans
+}
+
+export function Substraction(no1:number,no2:number):number
+{
+    var ans=0
+    ans=no1-no2
+    return ans
+}
