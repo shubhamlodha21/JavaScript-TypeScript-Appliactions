e3fb6d6cdce653c8362909c68982b58db0d36d84
Shubham Lodha <83054906+shubhamlodha21@users.noreply.github.com>
2021-09-09 20:22:06 +0530

    Display name by function creation

---
+++ Demo.js
@@ -0,0 +1,13 @@
+////////////////////////////////////////////////////////
+//Function Name:Display
+//Description:Display Name by using Display Function
+//Date: 09/09/2021
+//Author: Shubham Lodha
+///////////////////////////////////////////////////////
+
+function Display(name)
+{
+	console.log("Hello "+name);
+}
+
+Display("Shubham");
