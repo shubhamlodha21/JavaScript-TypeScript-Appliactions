a5192fa29100a1624fabb465530f8599d0a4d18c
Shubham Lodha <83054906+shubhamlodha21@users.noreply.github.com>
2023-01-03 23:21:01 +0530

    Datatypes in JS

---
+++ JavaScript Applications/Datatypes.js
@@ -0,0 +1,16 @@
+//DataTypes in JavaScript
+
+let length = 21;
+let lastName = "Lodha";
+let y = false;
+let z 
+let a = null
+
+const person = {firstName:"John", lastName:"Doe"};
+
+console.log(typeof(length))
+console.log(typeof(lastName))
+console.log(typeof(y))
+console.log(typeof(z))
+console.log(typeof(a))
+console.log(typeof(person))
